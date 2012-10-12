b2403eac027711e525ad219351e0aa943aab7016
Heriberto Perez & Victor Velazquez <pair+heriberto+victor@crowdint.com>
2012-10-12 17:02:10 -0500

    Create a Rails Project
    * Add git init configuration
    * Add git pair gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminHr::Application.config.secret_token = '68140de9f756fae5eb46d4e11be0bffd88fc9efd6a3c1e719fe6c5b1f2852f081c5202c3bb5c803bcdbdf092a2dbbd572e65c718f7c2bbc364e8a13ed769ae9b'
