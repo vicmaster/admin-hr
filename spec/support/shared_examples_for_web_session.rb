07c82b70bc407daca51f7e58fd27f9d736897db4
Heriberto Perez & Victor Velazquez <pair+heriberto+victor@crowdint.com>
2012-10-13 12:08:03 -0500

    Implement stylesheets to navbar
    * Add current_admin variable in layout

--- spec/support/shared_examples_for_web_session.rb
+++ spec/support/shared_examples_for_web_session.rb
@@ -10,9 +10,7 @@ shared_examples "web session" do
 
     fill_in "admin[email]",    with: user.email
     fill_in "admin[password]", with: user.password
-    save_and_open_page
     click_button "Sign in"
-    save_and_open_page
 
     page.should have_content('Sign out')
     current_path.should == root_path
